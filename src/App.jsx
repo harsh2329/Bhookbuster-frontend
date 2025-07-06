48fdec5d754a34de38175b83313d3aaa9ff06522
harsh2329 <hsoni2392003@gmail.com>
2025-07-06 16:29:27 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,43 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Usersidebar from './components/user/UserSidebar';
+import UserSidebar from './components/user/UserSidebar';
 import UserSignup from './components/common/UserSignup';
 import UserLogin from './components/common/UserLogin';
-import Rsidebar from './components/Restuarant/Rsidebar';
-// import Adminsidebar from './components/Admin/Adminsidebar';
+import RSidebar from './components/restaurant/RSidebar';
+// import AdminSidebar from './components/Admin/AdminSidebar';
 import './assets/css/adminlte.css';
 import './assets/css/adminlte.min.css';
-import Rlogin from './components/Restuarant/Rlogin';
-import Rsignup from './components/Restuarant/Rsignup';
-// import Alogin from './components/Admin/Alogin';
+import RLogin from './components/restaurant/RLogin';
+import RSignup from './components/restaurant/RSignup';
+// import ALogin from './components/Admin/ALogin';
 import axios from 'axios';
 // import './App.css';
+// import './App.css';
 import { useEffect } from 'react';
-import LandingPage from './components/Landing page/LandingPage';
+import LandingPage from './components/landingPage/LandingPage';
 import PrivateRoutes from "./hooks/PrivateRoutes";
-import LocationForm from "./components/Restuarant/LocationForm";
-import ResHero from "./components/Restuarant/ResHero";
-import Offer from "./components/Restuarant/Offer";
-import Rdashboard from "./components/Restuarant/Rdasborad";
-import Home from './components/Landing page/Navbar/Home';
-import Aboutus from './components/Landing page/Navbar/Aboutus';
-import Viewoffer from './components/Restuarant/Viewoffer';
-import ViewSingleOffer from './components/Restuarant/ViewSingleOffer';
-import AdminSidebar from './components/Admin/Adminsidebar';
+import LocationForm from "./components/restaurant/LocationForm";
+import ResHero from "./components/restaurant/ResHero";
+import Offer from "./components/restaurant/Offer";
+import RDashboard from "./components/restaurant/RDashboard";
+import Home from './components/landingPage/navbar/Home';
+import AboutUs from './components/landingPage/Navbar/AboutUs';
+import ViewOffer from './components/Restaurant/ViewOffer';
+import ViewSingleOffer from './components/Restaurant/ViewSingleOffer';
+import AdminSidebar from './components/Admin/AdminSidebar';
 import AdminDashboard from './components/Admin/AdminDashboard';
 
-import OffersPage from './components/Landing page/OffersPage';
-import FoodPage from './components/Landing page/FoodPage';
+import OffersPage from './components/landingPage/OffersPage';
+import FoodPage from './components/landingPage/FoodPage';
 import ResetPassword  from "../../bhookbustermain/src/components/common/ResetPassword";
 
-import ViewRestaurant from "../../bhookbustermain/src/components/Restuarant/ViewRestaurant";
-import ViewUser from "../../bhookbustermain/src/components/user/ViewUser";
+import ViewRestaurant from "./components/restaurant/ViewRestaurant";
+import ViewUser from "./components/user/ViewUser";
 import AdminLogin from './components/Admin/AdminLogin';
 import AdminLogout from './components/Admin/AdminLogout';
-import ResHome from '../../bhookbustermain/src/components/Restuarant/ResHome';
-import ViewResParti from '../../bhookbustermain/src/components/Restuarant/ViewResParti';
-import FirmCollections from '../src/components/Landing page/FirmCollections';
+import ResHome from './components/restaurant/ResHome';
+import ViewResParti from './components/restaurant/ViewResParti';
+import FirmCollections from './components/landingPage/FirmCollections';
 
 
 function App() {
@@ -61,65 +62,65 @@ function App() {
     <div className="layout-fixed sidebar-expand-lg bg-body-tertiary app-loaded">
       <div className="app-wrapper">
         <Routes>
-        <Route path='/login' element={<UserLogin />} />
-        <Route path='/signup' element={<UserSignup />} />
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/locationform' element={<LocationForm />} />
-        <Route path='/herores' element={<ResHero />} />
-        <Route path='/offer' element={<Offer />} />
-        <Route path ='/rdashboard' element={<Rdashboard />} />
-        <Route path="/resetpassword/:token" element={<ResetPassword />} />
-        <Route path="/home" element={<Home />} />
-        <Route path='/aboutus' element={<Aboutus />} />
-        <Route path="/viewoffer" element={<Viewoffer />} />
-        <Route path='rlogin' element={<Rlogin />} />
-        <Route path='rsignup' element={<Rsignup />} />
-        <Route path='singleoffer' element={<ViewSingleOffer />} />
-        <Route path ='/admin' element={<AdminDashboard />} />
-        <Route path ='/viewrestaurant' element={<ViewRestaurant />} />
-        <Route path='/viewuser' element={<ViewUser />} />
-        <Route path='/viewpartires' element={<ViewResParti />} />
-        <Route path='/rescarddisplay' element={<FirmCollections />} />
+  <Route path='/login' element={<UserLogin />} />
+  <Route path='/signup' element={<UserSignup />} />
+  <Route path='/' element={<LandingPage />} />
+  <Route path='/locationform' element={<LocationForm />} />
+  <Route path='/herores' element={<ResHero />} />
+  <Route path='/offer' element={<Offer />} />
+  <Route path ='/rdashboard' element={<RDashboard />} />
+  <Route path="/resetpassword/:token" element={<ResetPassword />} />
+  <Route path="/home" element={<Home />} />
+  <Route path='/aboutus' element={<AboutUs />} />
+  <Route path="/viewoffer" element={<ViewOffer />} />
+  <Route path='rlogin' element={<RLogin />} />
+  <Route path='rsignup' element={<RSignup />} />
+  <Route path='singleoffer' element={<ViewSingleOffer />} />
+  <Route path ='/admin' element={<AdminDashboard />} />
+  <Route path ='/viewrestaurant' element={<ViewRestaurant />} />
+  <Route path='/viewuser' element={<ViewUser />} />
+  <Route path='/viewpartires' element={<ViewResParti />} />
+  <Route path='/rescarddisplay' element={<FirmCollections />} />
+
+  <Route path="admin/alogin" element={<AdminLogin />} />
+  <Route 
+    path="/admin/logout" 
+    element={
+      <ProtectedRoute>
+        <AdminLogout />
+      </ProtectedRoute>
+    } 
+  />
+  <Route path="*" element={<Navigate to="/admin/login" />} />
+  <Route path="/reshome" element ={<ResHome />} />
+
+  <Route path="" element={<PrivateRoutes />}></Route>
  
-        <Route path="admin/alogin" element={<AdminLogin />} />
-        <Route 
-          path="/admin/logout" 
-          element={
-            <ProtectedRoute>
-              <AdminLogout />
-            </ProtectedRoute>
-          } 
-        />
-        <Route path="*" element={<Navigate to="/admin/login" />} />
-        <Route path="/reshome" element ={<ResHome />} />
-  
-        <Route path="" element={<PrivateRoutes />}></Route>
-       
-        <Route path="/food/:category" element={<FoodPage />} /> 
-        <Route path="/offers" element={<OffersPage />} /> 
-    
-          <Route path='/user' element={<Usersidebar />}>
-           
-          </Route>
-          <Route path='/restaurant' element={<Rsidebar />}>
-            <Route path='rlogin' element={<Rlogin />} />
-            <Route path='rsignup' element={<UserSignup />} />
-           
-          </Route>
-          {/* <Route path='/admin' element={<AdminDashboard />} > */}
-          <Route 
-          path="/admin" 
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          } 
-        />
-          <Route path='admins' element={<AdminSidebar />} />
-            {/* <Route path='alogin' element={<Alogin />} /> */}
-            <Route path='asignup' element={<UserSignup />} />
-              <Route path ='viewrestaurant' element={<ViewRestaurant />} />
-          {/* </Route> */}
+  <Route path="/food/:category" element={<FoodPage />} /> 
+  <Route path="/offers" element={<OffersPage />} /> 
+
+    <Route path='/user' element={<UserSidebar />}>
+     
+    </Route>
+    <Route path='/restaurant' element={<RSidebar />}>
+      <Route path='rlogin' element={<RLogin />} />
+      <Route path='rsignup' element={<UserSignup />} />
+     
+    </Route>
+    {/* <Route path='/admin' element={<AdminDashboard />} > */}
+    <Route 
+    path="/admin" 
+    element={
+      <ProtectedRoute>
+        <AdminDashboard />
+      </ProtectedRoute>
+    } 
+  />
+    <Route path='admins' element={<AdminSidebar />} />
+      {/* <Route path='alogin' element={<ALogin />} /> */}
+      <Route path='asignup' element={<UserSignup />} />
+        <Route path ='viewrestaurant' element={<ViewRestaurant />} />
+    {/* </Route> */}
         </Routes>
       </div>
     </div>
@@ -127,3 +128,5 @@ function App() {
 }
 
 export default App;
+
+// all changes made 
